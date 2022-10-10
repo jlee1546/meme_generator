9306289f03ad7c6a7efb6ccdc63355a0a7508639
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-10 13:52:17 -0400

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,12 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <div className="header--icon">
-        <img src="../images/troll.png" alt="troll" />
+      <div className="header--brand">
+        <img className="header--icon" src="../images/troll.png" alt="troll" />
+        <h1>Meme Generator</h1>
+      </div>
+      <div className="header--projectInfo">
+        <p>React Course - Project 3</p>
       </div>
     </header>
   );
