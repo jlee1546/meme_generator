f28d5d03432e1839473b3dfd7a785f046612552f
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-10 13:16:15 -0400

    Setting up meme_generator project

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header--icon">
+        <img src="../images/troll.png" alt="troll" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
